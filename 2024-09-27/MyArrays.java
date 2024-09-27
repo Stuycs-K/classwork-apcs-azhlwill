562a45b90026c4e88bdf9edf46bf6271b066a3cc
William Li <azhlwill@gmail.com>
2024-09-27 09:54:01 -0400

    Wrote first draft of returnCopy code.

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,29 @@
+public class MyArrays{
+  public static void main(String[] args){
+
+
+
+  }
+
+  //Write a function that returns a new array with the same values as the parameter ary.
+  public static int[] returnCopy(int[]ary){
+    int[] newArr = new int[ary.length];
+
+    for (int i = 0; i < ary.length; i++)
+    {
+      newArr[i] = ary[i];
+    }
+
+    return newArr;
+  }
+
+  //Write a function that returns a new array, that contains all of the values of ary1 followed by all of the values of ary2.
+  //The order of the values should remain the same.
+
+  public static int[] concatArray(int[]ary1,int[]ary2){
+
+
+  }
+
+
+}
