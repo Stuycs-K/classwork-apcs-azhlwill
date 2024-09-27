def609f7d660c972aa4958d21fdfa3ea4a43771b
William Li <azhlwill@gmail.com>
2024-09-27 10:09:28 -0400

    Added in aryToString code with a few test cases.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,10 +1,35 @@
 public class MyArrays{
   public static void main(String[] args){
 
-
-
+// 
+    int[] test1 = {1,2,3,4,5};
+    System.out.println(aryToString(test1));
+    int[] test2 = {};
+    System.out.println(aryToString(test2));
+    int[] test3 = {100,10,1,0,-5};
+    System.out.println(aryToString(test3));
   }
 
+// arrToString
+  public static String aryToString(int[] nums){
+    String empty = "[";
+
+    for (int i = 0; i < nums.length; i++)
+    {
+      if (i == nums.length - 1)
+      {
+        empty = empty + nums[i];
+      }
+      else
+      {
+        empty = empty + (nums[i] + ", ");
+      }
+    }
+
+    return empty + "]";
+  }
+
+/*
   //Write a function that returns a new array with the same values as the parameter ary.
   public static int[] returnCopy(int[]ary){
     int[] newArr = new int[ary.length];
@@ -20,10 +45,8 @@ public class MyArrays{
   //Write a function that returns a new array, that contains all of the values of ary1 followed by all of the values of ary2.
   //The order of the values should remain the same.
 
+/*
   public static int[] concatArray(int[]ary1,int[]ary2){
 
-
+*/
   }
-
-
-}
