19ff37278685fa69bd84deec4146a00f93f28571
azhlwill <azhlwill@gmail.com>
2024-10-16 00:24:33 -0400

    final touches and comments

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -1,6 +1,5 @@
 import java.lang.Math;
 public class Demo{
-
   /*
   Write this method third. Test it then move on
   to the others.
@@ -19,10 +18,10 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
-
+// equilateral triangle
     Point side1 = new Point(0,0);
     Point side2 = new Point(1,0);
-    Point side3 = new Point(0.5,Math.sqrt(0.75));
+    Point side3 = new Point(0.5, Math.sqrt(0.75));
     System.out.println(side1.distanceTo(side2));
     System.out.println(side1.distanceTo(side3));
     System.out.println(side2.distanceTo(side3));
