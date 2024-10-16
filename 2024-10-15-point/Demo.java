cc5da9944d4bd1b4d82b052098b9a8ceb40221d5
azhlwill <azhlwill@gmail.com>
2024-10-16 00:22:26 -0400

    tested equilateral triangle

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -14,13 +14,17 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p3);
-
     System.out.println( p3);
+    System.out.println(p4);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
 
-
-    System.out.println(p4);
+    Point side1 = new Point(0,0);
+    Point side2 = new Point(1,0);
+    Point side3 = new Point(0.5,Math.sqrt(0.75));
+    System.out.println(side1.distanceTo(side2));
+    System.out.println(side1.distanceTo(side3));
+    System.out.println(side2.distanceTo(side3));
   }
 }
