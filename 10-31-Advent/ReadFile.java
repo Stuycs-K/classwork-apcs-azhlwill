cf3f412ab75835fde0e10e758ebd0df21dc55954
azhlwill <azhlwill@gmail.com>
2024-11-05 19:29:36 -0500

    Print eveyr other line and lines with { only

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -10,19 +10,25 @@ public class ReadFile {
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
-      //CODE THAT SCANS THE FILE.
-      while(input.hasNext()){
-        System.out.println(input.next());
+/* PRINT EVERY OTHER LINE
+      while (input.hasNextLine()){
+        System.out.println(input.nextLine());
+        input.nextLine();
       }
       input.close();//releases the file from your program
+*/
+
+      while (input.hasNextLine()){
+        String temp = input.nextLine();
+        if (temp.indexOf("{") > -1){
+          System.out.println(temp);
+        }
+      }
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
       return; //you can return from a void function just don't put a value.
     }
-
-
-
   }
 }
