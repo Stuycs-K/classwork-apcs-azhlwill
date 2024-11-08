3a8d24ccdf10222cdafe630f5a837b8ab4c582ed
azhlwill <azhlwill@gmail.com>
2024-11-07 22:21:20 -0500

    tested each new variable for step 13

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,7 +1,7 @@
-/* 
+/*  STEP 8
 Overriding the getName() method changed the name to "The Mighty " + getName(). This shows that methods that are overridden use a new version of the method from the superclass.
 */
-/*
+/* STEP 10
 Test 3 doesn't work because the animal is not a bird. Bird is a subclass of animal which is why the 4th test works. Bird is-A Animal, but Animal is not a Bird.
 */
 public class Driver{
@@ -15,5 +15,12 @@ public class Driver{
     Bird b1 = new Bird("hiiiiiiiiii" , 2, "Jim", 4.5, "green");
     //Bird b2 = new Animal("hellooo", 40, "Rick");
     Animal a2 = new Bird("yesssssss", 16, "Will", 100.0, "purple");
+
+    System.out.println("a1: ");
+    a1.speak();
+    System.out.println("b1: ");   
+    b1.speak();
+    System.out.println("a2: ");
+    a2.speak();
   }
 }
