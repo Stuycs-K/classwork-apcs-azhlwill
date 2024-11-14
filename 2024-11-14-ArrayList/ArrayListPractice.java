2b3990e0c2c3a00fcb03e52b98e6bc5edb665019
William Li <azhlwill@gmail.com>
2024-11-14 10:13:54 -0500

    Put in skeleton code for reverse

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class ArrayListPractice{
   public static ArrayList<String> createRandomArray(int size){
     int addInt=0;
-    ArrayList<String> RandArray= new ArrayList<String>(size);
+    ArrayList<String> RandArray = new ArrayList<String>(size);
     for(int i=0; i<size; i++){
       addInt=(int)(Math.random()*11);
       if (addInt==0){
@@ -26,5 +26,12 @@ public class ArrayListPractice{
       }
     }
     System.out.println(original);
+  }
+
+  public static ArrayList<String> makeReversedList( ArrayList<String> original){
+  //return a new ArrayList that is in the reversed order of the original.
+    ArrayList<String> returnArray = new ArrayList<String>(original.size());
+
+    return returnArray;
   }
 }
