c50e1ddf19ea465ea67011fbe35bf6e5a99987d2
William Li <azhlwill@gmail.com>
2024-11-14 10:10:26 -0500

    Wrote replaceEmpty and tested it

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,5 +1,6 @@
 //Jai Shah and William Li
 import java.util.ArrayList;
+
 public class ArrayListPractice{
   public static ArrayList<String> createRandomArray(int size){
     int addInt=0;
@@ -14,5 +15,16 @@ public class ArrayListPractice{
       }
     }
     return RandArray;
+  }
+
+  public static void replaceEmpty( ArrayList<String> original){
+    //Modify the ArrayList such that it has all of the empty strings are
+    //replaced with the word "Empty".
+    for (int i = 0; i < original.size(); i++){
+      if (original.get(i).equals("")){
+        original.set(i, "Empty");
+      }
+    }
+    System.out.println(original);
   }
 }
