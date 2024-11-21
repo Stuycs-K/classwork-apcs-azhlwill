48b448f2505e3a93a21ae4726f36e900c13bc167
William Li <azhlwill@gmail.com>
2024-11-21 10:03:00 -0500

    fixed some

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -24,45 +24,73 @@ public class Day2{
         String instructions = input.nextLine();
         System.out.println(instructions + "\n");
 
+        System.out.println(keypad[xCor][yCor]);
+
+        for (int i = 0; i <instructions.length(); i++){
+          char single = instructions.charAt(i);
+          if (single == 'U'){
+            if (xCor != 0){xCor--;}
+          }
+          else if (single == 'D'){
+            if (xCor != 2){xCor++;}
+          }
+          else if (single == 'L'){
+            if (yCor != 0){yCor--;}
+          }
+          else{
+            if (yCor != 2){yCor++;}
+          }
+        }
+
+        System.out.println(keypad[xCor][yCor]);
+/*
         for (int i = 0; i < instructions.length(); i++){
           char single = instructions.charAt(i);
-          if (single.equals('U')){
+          if (single == 'U'){
             if (xCor == 0){
               xCor = 0;
+              System.out.println(keypad[xCor][yCor]);
             }
             else{
               xCor++;
+              System.out.println(keypad[xCor][yCor]);
             }
           }
 
-          if (single.equals('D')){
+          if (single == 'D'){
             if (xCor == 2){
               xCor = 2;
+              System.out.println(keypad[xCor][yCor]);
             }
             else{
               xCor--;
+              System.out.println(keypad[xCor][yCor]);
             }
           }
 
-          if (single.equals('L')){
+          if (single == 'L'){
             if (yCor == 0){
               yCor = 0;
+              System.out.println(keypad[xCor][yCor]);
             }
             else{
               yCor--;
+              System.out.println(keypad[xCor][yCor]);
             }
           }
 
-          if (single.equals('R')){
+          if (single == 'R'){
             if (yCor == 2){
               yCor = 2;
+              System.out.println(keypad[xCor][yCor]);
             }
             else{
               yCor++;
+              System.out.println(keypad[xCor][yCor]);
             }
           }
-
         }
+*/
 
       }
 
