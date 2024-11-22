e581922e6109f848b4fc604362cf79b6810545fa
William Li <azhlwill@gmail.com>
2024-11-22 00:19:38 -0500

    added a few notes

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -22,52 +22,56 @@ public class Day4{
             String[] alphabet = new String[]{"0th", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         /// 
             while (input.hasNextLine()){
-                String look = input.nextLine();
-                String checkSum = "";
+                String look = input.nextLine(); // get line
+                String checkSum = ""; // blank
                 int sectorID = Integer.parseInt(look.substring(look.length()-10, look.length()-7));
-                
+                // get ID if the rooms valid
 
-                int[] counts = new int[27];
+                int[] counts = new int[27]; // so i dont have to count a as 0
 
                 for (int i = 0; i < look.length() - 6; i++){ // 
                     for (int j = 0; j < alphabet.length; j++){
                         if (look.substring(i,i+1).equals(alphabet[j])){
-                            int beforeAdd = counts[j];
-                            counts[j] = beforeAdd + 1;
+                            int beforeAdd = counts[j]; // previous value
+                            counts[j] = beforeAdd + 1; // increase count of that letter by one
                         }
                     }
                 }
 
-                ArrayList<Integer> sortedCounts = new ArrayList<Integer>();
+                ArrayList<Integer> sortedCounts = new ArrayList<Integer>(); 
                 for (int z = 0; z < counts.length; z++){
-                    sortedCounts.add(counts[z]);
+                    sortedCounts.add(counts[z]); // copying to arraylist so i can use Collections
                 }
 
-                Collections.sort(sortedCounts);
-                Collections.reverse(sortedCounts);
-                System.out.println(sortedCounts.toString() + "\n");
+                Collections.sort(sortedCounts); // sort in decreasing order
+                Collections.reverse(sortedCounts); // flip so that its increasing order
 
                 ////////
-                for (int out = 0; out < 5; out++){
-                    for (int in = 0; in < counts.length; in++){
-                        if (sortedCounts.get(out) == counts[in]){
-                            checkSum += "" + alphabet[in];
+                for (int out = 0; out < 5; out++){ // check the top 5
+                    for (int in = 0; in < counts.length; in++){ // go into the original counts
+                        if (sortedCounts.get(out) == counts[in]){ // since the #s are the same, just check if its equal
+                            checkSum += "" + alphabet[in]; //update the checkSum
                             
                             counts[in] = 0;
+                            // if u dont set this to 0, once you reach the break
+                            // it will go back to the outer for loop and get stuck back in counts
+                            // same letter over and over again
+                            //need to set counts[in] to 0 so that it can continue from counts 1 to 26
 
                             break;
                         }
                     }
                 }
-                System.out.println(checkSum);
-                System.out.println(look.substring(look.length()-6, look.length()-1));
-                System.out.println(sectorID);
+                System.out.println(checkSum); // the created checkSum
+                System.out.println(look.substring(look.length()-6, look.length()-1)); // checkSum from actual String 
+                System.out.println(sectorID); // ID from earlier
 
                 /// 
-                if (checkSum.equals(look.substring(look.length()-6, look.length()-1))){
-                    sectorSum += sectorID;
+                if (checkSum.equals(look.substring(look.length()-6, look.length()-1))){ // validate checkSum
+                    sectorSum += sectorID; // add ID
                 }
-            }
+            } //after while loop
+        ///
             System.out.println(sectorSum);
         ////////////// 
 
