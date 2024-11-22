d7ea7b43665fd378387ee3d7a9fd48e8eddea06b
William Li <azhlwill@gmail.com>
2024-11-22 00:09:17 -0500

    finished part 1

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.*; 
 import java.util.*; 
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class Day4{
     public static void main(String[] args){
@@ -15,13 +17,18 @@ public class Day4{
             File file = new File(filename);
             Scanner input = new Scanner(file);
             int sectorSum = 0;
+            
         //////////////setup
             String[] alphabet = new String[]{"0th", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         /// 
             while (input.hasNextLine()){
+                String look = input.nextLine();
+                String checkSum = "";
+                int sectorID = Integer.parseInt(look.substring(look.length()-10, look.length()-7));
+                
+
                 int[] counts = new int[27];
 
-                String look = input.nextLine();
                 for (int i = 0; i < look.length() - 6; i++){ // 
                     for (int j = 0; j < alphabet.length; j++){
                         if (look.substring(i,i+1).equals(alphabet[j])){
@@ -31,10 +38,37 @@ public class Day4{
                     }
                 }
 
-                System.out.println(Arrays.toString(counts));
+                ArrayList<Integer> sortedCounts = new ArrayList<Integer>();
+                for (int z = 0; z < counts.length; z++){
+                    sortedCounts.add(counts[z]);
+                }
+
+                Collections.sort(sortedCounts);
+                Collections.reverse(sortedCounts);
+                System.out.println(sortedCounts.toString() + "\n");
 
                 ////////
+                for (int out = 0; out < 5; out++){
+                    for (int in = 0; in < counts.length; in++){
+                        if (sortedCounts.get(out) == counts[in]){
+                            checkSum += "" + alphabet[in];
+                            
+                            counts[in] = 0;
+
+                            break;
+                        }
+                    }
+                }
+                System.out.println(checkSum);
+                System.out.println(look.substring(look.length()-6, look.length()-1));
+                System.out.println(sectorID);
+
+                /// 
+                if (checkSum.equals(look.substring(look.length()-6, look.length()-1))){
+                    sectorSum += sectorID;
+                }
             }
+            System.out.println(sectorSum);
         ////////////// 
 
             input.close();
