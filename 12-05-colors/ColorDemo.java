e5fb0dc058fc841250a09345621ee49b1c4a8483
William Li <azhlwill@gmail.com>
2024-12-03 10:05:09 -0500

    started skeleton

---
+++ 12-05-colors/ColorDemo.java
@@ -0,0 +1,9 @@
+
+public class ColorDemo{
+  public static void main(String[] args){
+    for (int i = 0; i < 20; i++){
+      System.out.print("\u001b[34mHi");
+      System.out.print("\u001b[37mHi");
+    }
+  }
+}
