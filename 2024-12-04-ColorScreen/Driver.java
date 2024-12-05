203330ee6d745f7c0db87ecb0f325d16361ff7f0
William Li <azhlwill@gmail.com>
2024-12-05 01:53:56 -0500

    got the numbers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,38 +1,42 @@
-import java.util.Random;
-import java.util.Arrays;
-
 public class Driver{
     public static void main(String[] args){
-        border(80,31);
+        System.out.print(Text.CLEAR_SCREEN);
+        int width = 80; 
+        int height = 31;
+        border(width, height);
         int[] nums = randomArray();
-        System.out.println(Arrays.toString(nums));
 
-        int column = 0;
+        int column = 23;
         for (int i = 0; i < 3; i++){
-            column += 20;
-            
             if (nums[i] < 25){
-                Text.color(Text.RED, Text.BRIGHT);
+                Text.color(Text.RED, Text.BRIGHT, Text.background(Text.BLACK));
             }
             else if (nums[i] > 75){
-                Text.color(Text.GREEN, Text.BRIGHT);
+                Text.color(Text.GREEN, Text.BRIGHT, Text.background(Text.BLACK));
             }
             else{
-                Text.color(Text.WHITE);
+                Text.color(Text.WHITE, Text.background(Text.BLACK));
             }
+            Text.go(2, column);
+            System.out.print(nums[i]);
+            column += 21;
         }
+
+        Text.go(32,0);
+        System.out.print(Text.RESET);
     }
 
     private static void border(int width, int height){
-        for (int i = 0; i <= height; i ++){
-            for (int j = 0; j <= width; j++){
-                if (i == 0 || i == height || j == 0 || j == width){
+        for (int i = 1; i <= height; i ++){
+            for (int j = 1; j <= width; j++){
+                Text.go(i,j);
+                if (i == 1 || i == height || j == 1 || j == width){
                     Text.color(Text.background(Text.RED));
                     System.out.print("+");
                 }
-                else if (i == 2){
+                else if (i == 3){
                     Text.color(Text.background(Text.RED));
-                    System.out.print("-");
+                    System.out.print("/");
                 }
                 else{
                     Text.color(Text.background(Text.BLACK));
