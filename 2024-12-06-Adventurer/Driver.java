422cf2951d27ba6c0351e20522bff64bcc047e2f
William Li <azhlwill@gmail.com>
2024-12-07 17:16:22 -0500

    wrote the tests

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,41 @@
 public class Driver{
     public static void main(String[] args){
-        System.out.println("temp");
+        Adventurer p1 = new Spearman("Bobby", 12);
+        Adventurer p2 = new Spearman("Bob", 10);
+
+        System.out.println("Match: ");
+        System.out.println(p1.getName() + "'s HP: " + p1.getHP());
+        System.out.println(p2.getName() + "'s HP: " + p2.getHP());
+
+        System.out.println("\nDefault attack:");
+        System.out.println(p1.attack(p2));
+        System.out.println(p2.attack(p1));
+
+        System.out.println("\nSupport:");
+        System.out.println(p2.support());
+        System.out.println(p2.getName() + " now has " + p2.getHP() + " HP.");
+        System.out.println("For some reason, " + p1.support(p2));
+        System.out.println("\n" + p2.getName() + " now has " + p2.getHP() + " HP.");
+        System.out.println(p1.getName() + " now has " + p1.getHP() + " HP.");
+
+        System.out.println("\nSpecial attack");
+        System.out.println(p1.specialAttack(p2));
+        System.out.println(p1.getName() + " now has " + p1.getSpecial() + " energy left.");
+        System.out.println(p2.getName() + " now has " + p2.getHP() + " HP.");
+        System.out.println(p2.specialAttack(p1));
+        System.out.println(p2.getName() + " now has " + p2.getSpecial() + " energy left.");
+        System.out.println(p1.getName() + " now has " + p1.getHP() + " HP.");
+        System.out.println(p1.getName() + " is DEAD!");
+
+        System.out.println("\nRESULT:");
+        if (p2.getHP() > 0){
+            System.out.println(p2.getName() + " IS THE WINNER!");
+        }
+        else{
+            System.out.println(p1.getName() + " IS THE WINNER!");
+        }
+
+        
+
     }
 }
