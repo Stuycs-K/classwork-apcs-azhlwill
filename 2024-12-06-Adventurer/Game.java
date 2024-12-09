56236e38af62578f9a457f9bf6c9f03d485db3ce
William Li <azhlwill@gmail.com>
2024-12-09 10:11:34 -0500

    made the loop and basic stats

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -5,11 +5,22 @@ class Game {
     Scanner userInput = new Scanner(System.in);
 
     //You can do the rest many times:
+    Adventurer p1 = new Spearman("Bobby", 12);
+    Adventurer p2 = new CodeWarrior("Bob", 10);
 
-    System.out.println("Enter username");
+
+    System.out.println("Would you like to play? y/n");
     //Read one line of user input
-    String userName = userInput.nextLine();
+    String runGame = userInput.nextLine();
+    while (runGame.equals("y")){
+      System.out.println("running game");
+
+      System.out.println("Current Stats:");
+      System.out.println(p1.getName() + "\n     HP: " + p1.getHP() + "/" + p1.getmaxHP() + "\n     " + p1.getSpecialName() + p1.getSpecial() + "/" + p1.getSpecialMax());
+
+      System.out.println("Would you like to continue? y/n");
+      runGame = userInput.nextLine();
+    }
     //Do something with the input
-    System.out.println("Username is: " + userName);
   }
 }
